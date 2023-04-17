fb8e215b2f309f20fdede2796498fc2b0f326766
Mikael Andres <msandres123@gmail.com>
2023-04-17 14:21:56 -0400

    new models

--- c:workspace/littlelemon/restaurant/models.py
+++ c:workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=0)
+    booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+   title = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   inventory = models.IntegerField(default=0) 
+
+   def __str__(self):
+      return self.title
